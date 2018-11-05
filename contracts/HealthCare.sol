13c46a34e4f0de28ff0846270f83f020d67f095d
Rishabh42 <rishabhthaney@gmail.com>
2018-11-05 16:10:13 +0530

    changed struct, added display fx, etc

--- contracts/HealthCare.sol
+++ contracts/HealthCare.sol
@@ -6,35 +6,42 @@ contract HealthCare {
     address labAdmin;
 
     struct Record {
+        uint256 ID;
         string testName;
-        uint date;
+        string date;
         string hospitalName;
         uint256 price;
-        bool hospitalFlag;
-        bool labFlag;
+        uint8 signatureCount;
+        mapping (address => uint8) signatures;
     }
 
-    modifier hAdmin {
-        if (msg.sender != hospitalAdmin) throw;
+    modifier hospitalOnly {
+        require (msg.sender == hospitalAdmin);
         _;
     }
 
-    modifier lAdmin {
-      if (msg.sender != labAdmin) throw;
-      _;
+    modifier labOnly {
+        require (msg.sender == labAdmin);
+        _;
     }
 
-    mapping (address => Record) records;
+    mapping (uint256=> Record) records;
+    Record[] public recordsArr;
 
-    function newRecord (string _tName, uint _date, string hName, uint256 price) public{
+    function newRecord (string _tName, string _date, string hName, uint256 price) public{
         Record memory newRecord = Record ({
+            ID: recordsArr.length + 1 ,
             testName: _tName,
             date: _date,
             hospitalName: hName,
             price: price,
-            hospitalFlag: false,
-            labFlag: false
+            signatureCount: 0
         });
+        records[newRecord.ID] = newRecord;
+        recordsArr.push(newRecord);
     }
 
+    function getUnsignedRecords(uint256 _ID) public returns (uint256, string, string, string, uint256){
+        return (records[_ID].ID, records[_ID].testName, records[_ID].date, records[_ID].hospitalName, records[_ID].price);
+    }
 }
