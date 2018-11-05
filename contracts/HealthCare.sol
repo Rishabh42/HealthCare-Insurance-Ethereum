b628dbc535c0cccc0d2cb7649c3b26805d7d092d
Rishabh42 <rishabhthaney@gmail.com>
2018-11-05 21:19:13 +0530

    logical changes

--- contracts/HealthCare.sol
+++ contracts/HealthCare.sol
@@ -1,9 +1,9 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.25;
 
 contract HealthCare {
 
-    address hospitalAdmin;
-    address labAdmin;
+    address private hospitalAdmin;
+    address private labAdmin;
 
     struct Record {
         address pAddr;
@@ -16,33 +16,32 @@ contract HealthCare {
         mapping (address => uint8) signatures;
     }
 
-    event recordCreated(uint256 ID, string testName, string date, string hospitalName, uint256 price);
-
     modifier signOnly {
         require (msg.sender == hospitalAdmin || msg.sender == labAdmin );
         _;
     }
 
-    mapping (uint256=> Record) _records;
-    Record[] public recordsArr;
-    Record[] public verifiedRecord;
+    mapping (uint256=> Record) public _records;
+    uint256[] public recordsArr;
 
-    function newRecord (string _tName, string _date, string hName, uint256 price) public{
-        Record memory newRecord = Record ({
+    event recordCreated(uint256 ID, string testName, string date, string hospitalName, uint256 price);
+
+    function newRecord (uint256 _ID, string _tName, string _date, string hName, uint256 price) public{
+        Record memory _newrecord = Record ({
             pAddr: msg.sender,
-            ID: recordsArr.length + 1 ,
+            ID: _ID,
             testName: _tName,
             date: _date,
             hospitalName: hName,
             price: price,
             signatureCount: 0
         });
-        _records[newRecord.ID] = newRecord;
-        recordsArr.push(newRecord);
-        recordCreated(newRecord.ID, _tName, _date, hName, price);
+        _records[_ID] = _newrecord;
+        recordsArr.push(_newrecord.ID);
+        emit  recordCreated(_newrecord.ID, _tName, _date, hName, price);
     }
 
-    function getUnsignedRecords(uint256 _ID) public returns (uint256, string, string, string, uint256){
+    function getUnsignedRecords(uint256 _ID) public view returns (uint256, string, string, string, uint256){
         return (_records[_ID].ID, _records[_ID].testName, _records[_ID].date, _records[_ID].hospitalName, _records[_ID].price);
     }
 
