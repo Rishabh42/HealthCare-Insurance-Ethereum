378d046dd0b59e5fe0836fce24a882ae5631c880
Rishabh Thaney <rishabhthaney@gmail.com>
2019-08-10 18:17:36 +0530

    Added comments

--- contracts/HealthCare.sol
+++ contracts/HealthCare.sol
@@ -22,23 +22,25 @@ contract HealthCare {
         _;
     }
 
+    constructor() public {
+        hospitalAdmin = msg.sender;
+        labAdmin = 0x7fAA47fc6F7027feF185c766f9c3C0cb5d8D8850;     //assigning a hard coded address from ganache                   
+    }
+    
+    
+    // Mapping to store records
     mapping (uint256=> Record) public _records;
     uint256[] public recordsArr;
 
-      constructor() public {
-
-        hospitalAdmin = msg.sender;
-        labAdmin = 0x7fAA47fc6F7027feF185c766f9c3C0cb5d8D8850;                       //enter address
-    }
-
     event recordCreated(uint256 ID, string testName, string date, string hospitalName, uint256 price);
     event recordSigned(uint256 ID, string testName, string date, string hospitalName, uint256 price);
-
+    
+    // Create new record
     function newRecord (uint256 _ID, string _tName, string _date, string hName, uint256 price) public{
         Record storage _newrecord = _records[_ID];
 
-         require(!_records[_ID].isValue);
-
+        // Only allows new records to be created
+        require(!_records[_ID].isValue);
             _newrecord.pAddr = msg.sender;
             _newrecord.ID = _ID;
             _newrecord.testName = _tName;
@@ -52,22 +54,21 @@ contract HealthCare {
         emit  recordCreated(_newrecord.ID, _tName, _date, hName, price);
     }
 
-   /* function getRecords(uint256 _ID) public view returns (uint256, string, string, string, uint256, uint256){
-        return (_records[_ID].ID, _records[_ID].testName, _records[_ID].date, _records[_ID].hospitalName, _records[_ID].price, _records[_ID].signatureCount);
-    } */
-
+    // Function to sign a record 
     function signRecord(uint256 _ID) signOnly public {
         Record storage records = _records[_ID];
-
+        
+        // Checks the aunthenticity of the address signing it
         require(address(0) != records.pAddr);
-
         require(msg.sender != records.pAddr);
-
+        
+        // Doesn't allow the same person to sign twice
         require(records.signatures[msg.sender] != 1);
 
         records.signatures[msg.sender] = 1;
         records.signatureCount++;
 
+        // Checks if the record has been signed by both the authorities to process insurance claim
         if(records.signatureCount == 2)
             emit  recordSigned(records.ID, records.testName, records.date, records.hospitalName, records.price);
 
