fc59f6860f88b47ac4d7ae370cf4920278090586
Nikunj Prajapati <nikunjprajapati0016@gmail.com>
2022-06-20 22:35:54 +0530

    Added reference comment

--- migrations/2_deploy_HealthCare.js
+++ migrations/2_deploy_HealthCare.js
@@ -1,5 +1,5 @@
 var HealthCare = artifacts.require("./HealthCare.sol");
-const TO_LAB_ADMIN_ADDRESS = "0xF6F2F51c07e44efE7BC25E0EC6B332f39d930ac0";
+const TO_LAB_ADMIN_ADDRESS = "0xF6F2F51c07e44efE7BC25E0EC6B332f39d930ac0"; // hard coded address from Ganache
 
 module.exports = function(deployer) {
   deployer.deploy(HealthCare, TO_LAB_ADMIN_ADDRESS);
