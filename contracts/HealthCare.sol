4d264217816e63d1f0c49895ac872e8e07b748e4
Nikunj R. Prajapati <46849277+LEO1612D@users.noreply.github.com>
2022-06-22 22:36:09 +0530

    Health care contract improvements (#35)
    * Health care contract improvements
    
    * Version update and small changes
    
    * More small formatting update
    
    * Version upgrade : Migratino contract
    
    * Integrated latest changes for contract deployment
    
    * Added reference comment
    
    * Updated README file
    
    * Revert README changes
    
    Co-authored-by: Nikunj Prajapati <nikunj.prajapati@dreamorbit.com>

--- contracts/HealthCare.sol
+++ contracts/HealthCare.sol
@@ -1,7 +1,7 @@
-pragma solidity >=0.4.22 <0.6.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 contract HealthCare {
-
     address private hospitalAdmin;
     address private labAdmin;
 
@@ -17,54 +17,64 @@ contract HealthCare {
         mapping (address => uint256) signatures;
     }
 
-    modifier signOnly {
-        require (msg.sender == hospitalAdmin || msg.sender == labAdmin );
-        _;
+    constructor(address _labAdmin) {
+        hospitalAdmin = msg.sender;
+        labAdmin = _labAdmin;
     }
 
-    constructor() public {
-        hospitalAdmin = msg.sender;
-        labAdmin = 0xF6F2F51c07e44efE7BC25E0EC6B332f39d930ac0;     //assigning a hard coded address from ganache                   
-    }
-    
-    
     // Mapping to store records
     mapping (uint256=> Record) public _records;
     uint256[] public recordsArr;
 
     event recordCreated(uint256 ID, string testName, string date, string hospitalName, uint256 price);
     event recordSigned(uint256 ID, string testName, string date, string hospitalName, uint256 price);
-    
-    // Create new record
-    function newRecord (uint256 _ID, string memory _tName, string memory _date, string memory hName, uint256 price) public{
-        Record storage _newrecord = _records[_ID];
 
+    modifier signOnly {
+        require (msg.sender == hospitalAdmin || msg.sender == labAdmin, "You are not authorized to sign this.");
+        _;
+    }
+
+    modifier checkAuthBeforeSign(uint256 _ID) {
+        require(_records[_ID].isValue, "Recored does not exist");
+        require(address(0) != _records[_ID].pAddr, "You are not authorized person to perform this action.");
+        require(msg.sender != _records[_ID].pAddr, "You are not authorized person to perform this action.");
+        require(_records[_ID].signatures[msg.sender] != 1, "Same person cannot sign twice.");
+        _;
+
+    }
+
+    modifier validateRecord(uint256 _ID) {
         // Only allows new records to be created
-        require(!_records[_ID].isValue);
-            _newrecord.pAddr = msg.sender;
-            _newrecord.ID = _ID;
-            _newrecord.testName = _tName;
-            _newrecord.date = _date;
-            _newrecord.hospitalName = hName;
-            _newrecord.price = price;
-            _newrecord.isValue = true;
-            _newrecord.signatureCount = 0;
+        require(!_records[_ID].isValue, "Record with this ID is already exist, you cannot create new record with the same ID");
+        _;
+    }
+
+    // Create new record
+    function newRecord (
+        uint256 _ID,
+        string memory _tName,
+        string memory _date,
+        string memory hName,
+        uint256 price
+    )
+    validateRecord(_ID) public {
+        Record storage _newrecord = _records[_ID];
+        _newrecord.pAddr = msg.sender;
+        _newrecord.ID = _ID;
+        _newrecord.testName = _tName;
+        _newrecord.date = _date;
+        _newrecord.hospitalName = hName;
+        _newrecord.price = price;
+        _newrecord.isValue = true;
+        _newrecord.signatureCount = 0;
 
         recordsArr.push(_ID);
         emit  recordCreated(_newrecord.ID, _tName, _date, hName, price);
     }
 
-    // Function to sign a record 
-    function signRecord(uint256 _ID) signOnly public {
+    // Function to sign a record
+    function signRecord(uint256 _ID) signOnly checkAuthBeforeSign(_ID) public {
         Record storage records = _records[_ID];
-        
-        // Checks the aunthenticity of the address signing it
-        require(address(0) != records.pAddr);
-        require(msg.sender != records.pAddr);
-        
-        // Doesn't allow the same person to sign twice
-        require(records.signatures[msg.sender] != 1);
-
         records.signatures[msg.sender] = 1;
         records.signatureCount++;
 
--- migrations/2_deploy_HealthCare.js
+++ migrations/2_deploy_HealthCare.js
@@ -1,5 +1,6 @@
 var HealthCare = artifacts.require("./HealthCare.sol");
+const TO_LAB_ADMIN_ADDRESS = "0xF6F2F51c07e44efE7BC25E0EC6B332f39d930ac0"; // hard coded address from Ganache
 
 module.exports = function(deployer) {
-  deployer.deploy(HealthCare);
+  deployer.deploy(HealthCare, TO_LAB_ADMIN_ADDRESS);
 };
